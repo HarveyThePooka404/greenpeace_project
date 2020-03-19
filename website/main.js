c5f6a018eac69a1375d41d587599bb595eea6a89
HarveyThePooka404 <57190763+HarveyThePooka404@users.noreply.github.com>
2020-03-19 18:00:15 +0100

    parallax opening weird

--- website/main.js
+++ website/main.js
@@ -1,3 +1,15 @@
+    const background = document.querySelector(".img-parallax");
+    const second = document.querySelector(".AboutUs");
+
+
+/*window.addEventListener("scroll", function (){
+    var scrolled = window.scrollY;
+    var offSet = - scrolled * 0.2 + 20;
+    var offSetSecond = scrolled * 0.1;
+    background.style.top = offSet + 'px';
+    second.style.top = offSetSecond + 'px';
+})*/
+
 const vid = document.querySelector("video");
 const btn = document.querySelector("button");
 const titleHeader = document.querySelector(".HeaderTitle");
@@ -14,3 +26,6 @@ function HeaderOut() {
 }
 
 vid.onended = HeaderOut;
+
+// starting for the parallax
+
