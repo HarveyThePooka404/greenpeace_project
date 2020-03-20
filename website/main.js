fd2e1cc4831cb68ba355391b8fa9620e3343e070
HarveyThePooka404 <57190763+HarveyThePooka404@users.noreply.github.com>
2020-03-20 12:26:27 +0100

    finished desktop parallax

--- website/main.js
+++ website/main.js
@@ -1,15 +1,3 @@
-    const background = document.querySelector(".img-parallax");
-    const second = document.querySelector(".AboutUs");
-
-
-/*window.addEventListener("scroll", function (){
-    var scrolled = window.scrollY;
-    var offSet = - scrolled * 0.2 + 20;
-    var offSetSecond = scrolled * 0.1;
-    background.style.top = offSet + 'px';
-    second.style.top = offSetSecond + 'px';
-})*/
-
 const vid = document.querySelector("video");
 const btn = document.querySelector("button");
 const titleHeader = document.querySelector(".HeaderTitle");
@@ -18,8 +6,8 @@ btn.addEventListener("click", HeaderOut);
 
 function HeaderOut() {
     console.log("look at that button");
-  vid.classList.add("disappear");
-  btn.classList.add("disappear");
+    vid.classList.add("disappear");
+    btn.classList.add("disappear");
 
     titleHeader.classList.remove("disappear");
     titleHeader.classList.add("AnimationHeader");
@@ -29,3 +17,35 @@ vid.onended = HeaderOut;
 
 // starting for the parallax
 
+// const background = document.querySelector(".img-parallax");
+// const second = document.querySelector(".AboutUs");
+
+const parallaxedDown = document.querySelectorAll(".parallaxed-down");
+
+window.addEventListener("scroll", function () {
+
+    parallaxedDown.forEach(function (e) {
+        var scrolled = window.scrollY;
+        var offSet = scrolled * e.getAttribute("data-delay") - 220;
+        e.style.top = offSet + "px";
+
+    });
+
+    //background.style.top = offSet + 'px';
+    //second.style.top = offSetSecond + 'px';
+})
+
+const parallaxedUp = document.querySelectorAll(".parallaxed-up");
+
+window.addEventListener("scroll", function () {
+
+    parallaxedUp.forEach(function (f) {
+        var scrolled = window.scrollY;
+        var offSet = - scrolled * f.getAttribute("data-delay");
+        f.style.top = offSet + "px";
+
+    });
+
+    //background.style.top = offSet + 'px';
+    //second.style.top = offSetSecond + 'px';
+})
